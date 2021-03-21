27afa86f1bcec773fa6555f0bffd2e820f44e469
Gerardo Muñoz <gerardomx17@gmail.com>
2021-03-21 15:44:53 -0600

    Ajustando el Cabezote al diseño responsivo

--- vistas/modulos/cabezote.php
+++ vistas/modulos/cabezote.php
@@ -1,7 +1,7 @@
 <div class="container-fluid barraSuperior" id="top">
     <div class="container">
         <div class="row">
-            <div class="col-12 col-md-8 social">
+            <div class="col-12 col-sm-6 col-md-8 social">
                 <ul>
                     <li>
                         <a href="http://facebook.com/" target="_blank">
@@ -25,7 +25,7 @@
                     </li>
                 </ul>
             </div>
-            <div class="col-12 col-md-4 registro">
+            <div class="col-12 col-sm-6 col-md-4 registro">
                 <ul>
                     <li><a href="#modalIngreso" data-toggle="modal"><i class="fas fa-user"></i> Ingresar</a></li>
                     <li>|</li>
@@ -36,11 +36,11 @@
     </div>
 </div>
 
-<div class="container-fluid backColor">
+<div class="container-fluid backColor" id="menu">
     <nav class="container navbar navbar-expand-lg backColor">
-    <a class="navbar-brand" href="#">Navbar</a>
+        <img src="http://localhost/pelisxyz/backend/vistas/img/plantilla/logo.png" class="img-fluid" alt="Logo Pelis XYZ">
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
+        <span class="navbar-toggler-icon"><i class="fas fa-bars" style="color:#fff; font-size:28px;"></i></span>
     </button>
 
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
