ded98db2b7e84eba15f843899cc9413dd4a85ae8
Gerardo Muñoz <gerardomx17@gmail.com>
2021-03-20 19:40:59 -0600

    Maquetando el Cabezote con HTML5 - parte 1

---
+++ vistas/modulos/cabezote.php
@@ -0,0 +1,37 @@
+<div class="container-fluid barraSuperior" id="top">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-md-9 social">
+                <ul>
+                    <li>
+                        <a href="http://facebook.com/" target="_blank">
+                            <i class="fab fa-facebook-f redSocial facebookBlanco"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="http://instagram.com/" target="_blank">
+                            <i class="fab fa-instagram redSocial instagramBlanco"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="http://twitter.com/" target="_blank">
+                            <i class="fab fa-twitter redSocial twitterBlanco"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="http://youtube.com/" target="_blank">
+                            <i class="fab fa-youtube redSocial youtubeBlanco"></i>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-12 col-md-3 registro">
+                <ul>
+                    <li><a href="#modalIngreso" data-toggle="modal"><i class="fas fa-user"></i> Ingresar</a></li>
+                    <li>|</li>
+                    <li><a href="#modalRgistro" data-toggle="modal"><i class="fas fa-user-plus"></i> Crear una cuenta</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
